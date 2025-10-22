a5deeeecd9291d0fc33eb104085cda13d5983ccc
Lou Lindsley <4loubird@gmail.com>
2025-10-22 09:53:21 -0400

    added start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+echo "Starting Docker container..."
+cd ~/bios611/hw4_ufo/
+
+docker build . --platform=linux/amd64 -t r-cluster-analysis
+docker run\
+  --platform=linux/amd64\
+  -v $(pwd):/home/rstudio/work\
+  -v $HOME/.ssh:/home/rstudio/.ssh\
+  -v $HOME/.gitconfig:/home/rstudio/.gitconfig\
+  -e PASSWORD=123\
+  -p 8787:8787 -it r-cluster-analysis
+
