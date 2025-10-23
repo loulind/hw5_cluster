80b742671fbb074864d3b0e7bbfdf27266567e7e
Lou Lindsley <4loubird@gmail.com>
2025-10-23 10:53:29 -0400

    created clustering script

---
+++ cluster_kmeans.R
@@ -0,0 +1,8 @@
+# This script conducts kmeans clustering on the TASK 1 data
+library(tidyverse)
+
+df_n2 <- read_csv("work/task1data/df_n2")
+df_n3 <- read_csv("work/task1data/df_n3")
+df_n4 <- read_csv("work/task1data/df_n4")
+df_n5 <- read_csv("work/task1data/df_n5")
+df_n6 <- read_csv("work/task1data/df_n6")
