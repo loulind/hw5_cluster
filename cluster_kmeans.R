3584f2c679586a7ab92c798b071230d0332c3df9
Lou Lindsley <4loubird@gmail.com>
2025-10-28 15:06:54 -0400

    Added .csv to file exports

--- cluster_kmeans.R
+++ cluster_kmeans.R
@@ -2,11 +2,11 @@
 library(tidyverse)
 library(cluster)
 
-df_n2 <- read_csv("work/task1data/df_n2")
-df_n3 <- read_csv("work/task1data/df_n3")
-df_n4 <- read_csv("work/task1data/df_n4")
-df_n5 <- read_csv("work/task1data/df_n5")
-df_n6 <- read_csv("work/task1data/df_n6")
+df_n2 <- read_csv("~/work/task1data/df_n2.csv")
+df_n3 <- read_csv("~/work/task1data/df_n3.csv")
+df_n4 <- read_csv("~/work/task1data/df_n4.csv")
+df_n5 <- read_csv("~/work/task1data/df_n5.csv")
+df_n6 <- read_csv("~/work/task1data/df_n6.csv")
 
 list_of_dfs <- list(df_n2, df_n3, df_n4, df_n5, df_n6)
 
